fd6b642ba05c373c62c60d2be1e3070d93d3d234
xinyu <35090558+Erhao@users.noreply.github.com>
2021-08-10 22:58:16 +0800

    set no expire in create_access_token

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -77,10 +77,10 @@ async def get_current_user(
 
 def create_access_token(*, data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire, "sub": access_token_jwt_subject})
+    # if expires_delta:
+    #     expire = datetime.utcnow() + expires_delta
+    # else:
+    #     expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"sub": access_token_jwt_subject})
     encoded_jwt = jwt.encode(to_encode, str(SECRET_KEY), algorithm=ALGORITHM)
     return encoded_jwt
